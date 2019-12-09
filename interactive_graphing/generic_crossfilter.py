9a53c65c20049f5f981a323e382225047f6d2930
Mark Rossi <mark.rossi.06@gmail.com>
2019-12-09 16:30:23 -0500

    finish hover and add generic crossfilter

---
+++ interactive_graphing/generic_crossfilter.py
@@ -0,0 +1,112 @@
+from pprint import pprint
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# create a six-column dataset to plot across three scatter charts
+np.random.seed(0)
+df = pd.DataFrame({f"Col {i+1}" : np.random.rand(30) for i in range(6)})
+
+graph_config = {'displayModeBar': False}
+
+g1 = dcc.Graph(id='g1', config=graph_config)
+g2 = dcc.Graph(id='g2', config=graph_config)
+g3 = dcc.Graph(id='g3', config=graph_config)
+
+graphs = (g1, g2, g3)
+
+components = [html.Div(g, className='four columns') for g in graphs]
+
+app.layout = html.Div(components)
+
+def get_figure(df, x_col, y_col, selected_points, local_selection):
+    """
+    Updates selections in a scatter plot based on an input selection
+    and the current state of selected data in the plot.
+    """
+    
+    # if there is a local selection, find its bounds so we can persist a rectangle
+    if local_selection and local_selection['range']:
+        pprint(f'local_selection: {local_selection}')
+        ranges = local_selection['range']
+        selection_bounds = {
+            'x0': ranges.get('x')[0], 'x1': ranges.get('x')[1],
+            'y0': ranges.get('y')[1], 'y1': ranges.get('y')[1]
+        }
+    # otherwise set the bounds as the whole chart area
+    else:
+        selection_bounds = {
+            'x0': np.min(df[x_col]), 'x1': np.max(df[x_col]),
+            'y0': np.min(df[y_col]), 'y1': np.max(df[y_col])
+        }
+
+    data = [dict(
+        x=df[x_col],
+        y=df[y_col],
+        text=df.index,
+        textposition='top',
+        selectedpoints=selected_points,
+        customdata=df.index,
+        type='scatter',
+        mode='markers+text',
+        marker={'color': 'rgba(0, 116, 217, 0.7)', 'size': 12},
+        unselected={
+            'marker': {'opacity': 0.3},
+            'textfont': {'color': 'rbga(0, 0, 0, 0)'}
+        }
+    )]
+
+    layout = {
+        'margin': {'l': 20, 'r': 0, 'b': 15, 't': 5},
+        'dragmode': 'select',
+        'hovermode': False,
+        # Display a rectangle to cover the selection bounds
+        'shapes': [dict({
+            'type': 'rect',
+            'line': { 'width': 1, 'dash': 'dot', 'color': 'darkgrey' }
+        }, **selection_bounds
+        )]
+    }
+
+    return {
+        'data': data,
+        'layout': layout
+    }
+
+@app.callback(
+    [Output(g.id, 'figure') for g in graphs],
+    [Input(g.id, 'selectedData') for g in graphs]
+)
+def callback(selection1, selection2, selection3):
+    """
+    Update all three graphs whenever the selected points on any graph changes.
+
+    Takes the local selection from the graph that was changed, finds all points
+    contained within it, and calls get_figure() to update the other graphs to 
+    reflect the same selection.
+    """
+    
+    selected_points = df.index
+
+    for selected_data in [selection1, selection2, selection3]:
+        if selected_data and selected_data.get('points'):
+            # if there are points selected in any graph, 
+            selected_points = np.intersect1d(
+                selected_points, 
+                [p.get('customdata') for p in selected_data.get('points')]
+            )
+    
+    return [get_figure(df, "Col 1", "Col 2", selected_points, selection1),
+            get_figure(df, "Col 3", "Col 4", selected_points, selection2),
+            get_figure(df, "Col 5", "Col 6", selected_points, selection3)]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- interactive_graphing/update_on_hover.py
+++ interactive_graphing/update_on_hover.py
@@ -1,8 +1,18 @@
 """
 Dash Tutorial Ch. 5 - example 2/
+
+Reacting to "hover" events in a graph.
+
+This application creates the same graph with configurable axes from 
+the callbacks > multiple inputs example, and extends it by reacting
+to hover events in the graph. When a single country point is hovered, 
+time series graphs on the right of the screen will show the x- and y-axis 
+indicators for that country over all time in the dataset.
+
 """
 
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
@@ -11,5 +21,189 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-from callbacks import df, available_indicators
+df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
 
+available_indicators = df['Indicator Name'].unique()
+
+# crossfilter selectors
+x_axis_column = dcc.Dropdown(
+    id='crossfilter-xaxis-column',
+    options=[{'label': i, 'value': i} for i in available_indicators],
+    value='Fertility rate, total (births per woman)'
+)
+
+x_axis_type = dcc.RadioItems(
+    id='crossfilter-xaxis-type',
+    options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+    value='Linear',
+    labelStyle={'display': 'inline-block'}
+)
+
+y_axis_column = dcc.Dropdown(
+    id='crossfilter-yaxis-column',
+    options=[{'label': i, 'value': i} for i in available_indicators],
+    value='Life expectancy at birth, total (years)'
+)
+
+y_axis_type = dcc.RadioItems(
+    id='crossfilter-yaxis-type',
+    options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+    value='Linear',
+    labelStyle={'display': 'inline-block'}
+)
+
+crossfilter_style = {'width': '49%', 'display': 'inline-block'}
+container_style = {
+    'borderBottom': 'thin lightgrey solid',
+    'backgroundColor': 'rgb(250, 250, 250)',
+    'padding': '10px 5px'
+}
+
+crossfilter_div = html.Div([
+    html.Div([x_axis_column, x_axis_type], style=crossfilter_style),
+    html.Div([y_axis_column, y_axis_type], style=crossfilter_style)
+], style=container_style)
+
+
+# scatter plot
+scatter = dcc.Graph(
+    id='crossfilter-indicator-scatter',
+    hoverData={'points': [{'customdata': 'Japan'}]}
+)
+
+scatter_style = {'width': '49%', 'display': 'inline-block', 'padding': '0 20'}
+
+scatter_div = html.Div([scatter], style=scatter_style)
+
+# time series 
+x_time_series = dcc.Graph(id='x-time-series')
+
+y_time_series = dcc.Graph(id='y-time-series')
+
+ts_style = crossfilter_style
+
+ts_div = html.Div([x_time_series, y_time_series], style=ts_style)
+
+# year slider
+slider = dcc.Slider(
+    id='crossfilter-year-slider',
+    min=df['Year'].min(),
+    max=df['Year'].max(),
+    value=df['Year'].max(),
+    marks={str(year): str(year) for year in df['Year'].unique()},
+    step=None
+)
+
+slider_style = {'width': '49%', 'padding': '0px 20px 20px 20px'}
+
+slider_div = html.Div(slider, style=slider_style)
+
+components = [crossfilter_div, scatter_div, ts_div, slider_div]
+app.layout = html.Div(components)
+
+inputs = ['crossfilter-xaxis-column', 
+    'crossfilter-yaxis-column', 
+    'crossfilter-xaxis-type', 
+    'crossfilter-yaxis-type', 
+    'crossfilter-year-slider']
+
+marker = {
+    'size': 15,
+    'opacity': 0.5,
+    'line': {'width': 0.5, 'color': 'white'}
+}
+
+# same function as multi-input example from "Callbacks" chapter 
+@app.callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    [Input(i, 'value') for i in inputs]
+)
+def update_graph(x_axis_name, y_axis_name, x_axis_type, y_axis_type, year):
+    dff = df[df['Year']==year]
+
+    data = [dict(
+        x=dff[dff['Indicator Name']==x_axis_name].Value,
+        y=dff[dff['Indicator Name']==y_axis_name].Value,
+        text=dff[dff['Indicator Name']==x_axis_name]['Country Name'],
+        customdata=dff[dff['Indicator Name']==y_axis_name]['Country Name'],
+        mode='markers',
+        marker=marker
+    )]
+
+    layout = dict(
+        xaxis={
+            'title': x_axis_name,
+            'type': x_axis_type.lower(),
+        },
+        yaxis={
+            'title': y_axis_name,
+            'type': y_axis_type.lower()
+        },
+        margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+        height=450,
+        hovermode='closest',
+        transition={'duration': 500}
+    )
+
+    return {
+        'data': data,
+        'layout': layout
+    }
+
+# encapsulate logic to update both x- and y-axis time series
+def create_time_series(dff, axis_type, title):
+    
+    data = [{
+        'x': dff['Year'],
+        'y': dff['Value'],
+        'mode': 'lines+markers'
+    }]
+    
+    layout = {
+        'height': 225,
+        'margin': {'l': 20, 'b': 30, 'r': 10, 't': 10},
+        'annotations': [{
+            'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
+            'xref': 'paper', 'yref': 'paper', 'showarrow': False,
+            'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
+            'text': title
+        }],
+        'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
+        'xaxis': {'showgrid': False}
+    }
+
+    return {
+        'data': data,
+        'layout': layout
+    }
+
+# update the x-axis time series on hover in the main graph
+@app.callback(
+    Output('x-time-series', 'figure'),
+    [Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value')]
+)
+def update_x_timeseries(hoverData, axis_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == axis_name]
+    title = f'<b>{country_name}</b><br>{axis_name}'
+    return create_time_series(dff, axis_type, title)
+
+# update the y-axis time series on hover in the main graph
+@app.callback(
+    Output('y-time-series', 'figure'),
+    [Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-yaxis-column', 'value')]
+)
+def update_y_timeseries(hoverData, axis_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name']==country_name]
+    dff = dff[dff['Indicator Name'] == axis_name]
+    title = f'<b>{country_name}</b><br>{axis_name}'
+    return create_time_series(dff, axis_type, title)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
